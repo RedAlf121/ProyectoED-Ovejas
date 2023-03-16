5afd433ba7a173abae762da5623e0f577b338212
RedAlf121 <111950884+RedAlf121@users.noreply.github.com>
2023-03-15 20:17:05 -0400

    Mejorando el grafo
    Ahora las busquedas se hacen con tablas de hash. Y se simplificaron los metodos para construir el grafo

--- data structures/Graph/Graph.gd
+++ data structures/Graph/Graph.gd
@@ -1,77 +1,60 @@
 class_name Graph extends Node
 
+const _ways = [Vector2.LEFT,Vector2.RIGHT,Vector2.DOWN,Vector2.UP]
 
-var _nodes : Array
-var _rectangle #este atributo es auxiliar para saber las filas y las columnas a la hora de transformar el TileMap en Grafo
+var _nodes : Dictionary
 
 func build_graph(tile : TileMap, obstacles: TileMap):
 	var matrix = tile.get_used_cells()
-	_rectangle = tile.get_used_rect()
 	var cell_size = tile.cell_size
 	for i in matrix:
 		_build_node(i,cell_size,obstacles)
 	_connect_nodes()
 
+
 func _build_node(position,size,obstacles):
 		var node = GNode.new()
 		node.build(position,size)
 		node.be_bussy(obstacles)
-		_nodes.push_back(node)
+		_nodes[node.get_position()] = node
+
 
 func _connect_nodes():
-	for i in _nodes.size():
+	for i in _nodes.keys():
 		var position = _nodes[i].get_position()
-		_add_adyacency(_nodes[i], Vector2(position.x+1,position.y))
-		_add_adyacency(_nodes[i], Vector2(position.x-1,position.y))
-		_add_adyacency(_nodes[i], Vector2(position.x,position.y+1))
-		_add_adyacency(_nodes[i], Vector2(position.x,position.y-1))
+		for j in _ways:
+			_add_adyacency(_nodes[i], position+j)
+
 
 func _add_adyacency(node: GNode, position: Vector2):
 	var index = _find_node(position)
-	if(index != -1):
-		node.add_node(_nodes[index])
+	if(index != null):
+		node.add_node(index)
 
-func _find_node(position: Vector2):
-	var index = -1
-	if(_rectangle.has_point(position)):
-		index = position.y * _rectangle.size.x + position.x
-	return index
 
-func _find_node_binary_search(position: Vector2):
-	var index = -1
-	var li = 0
-	var ls = _nodes.size()-1
-	var mid
-	var node_position
-	while(li <= ls and index == -1):
-		mid = (li+ls)/2
-		node_position = _nodes[mid].get_position()
-		if(node_position == position):
-			index = mid
-		elif(node_position.y < position.y or (node_position.y == position.y and node_position.x < position.x)):
-			li = mid+1
-		else:
-			ls = mid-1
-	return index
+func _find_node(position: Vector2):	
+	return _nodes.get(position)
+
 
 func _transform(position):
-	var size = _nodes[0].size
+	var size = _nodes[_nodes.keys()[0]].size
 	return (position-size/2)/size
+
 
 func shortest_path(position_from, position_to):
 	var path = null
 	if !_nodes.empty():
-		var node_from_position = _find_node(_transform(position_from))
-		var node_to_position = _find_node(_transform(position_to))
-		if(node_from_position != -1 and node_to_position != -1):
-			var node_from = _nodes[node_from_position]
-			var node_to = _nodes[node_to_position]
+		var node_from = _find_node(_transform(position_from))
+		var node_to = _find_node(_transform(position_to))
+		if(node_from != null and node_to != null):
 			path = _shortest_path(node_from, node_to)
 	return path
+
 
 func _shortest_path(node_from, node_to):
 	var node_map = _bfs(node_from, node_to)
 	return _path(node_map, node_to)
+
 
 func _path(node_map, node):
 	var path = []
@@ -81,6 +64,7 @@ func _path(node_map, node):
 		path.push_front(node_map[aux_node])
 		aux_node = path[0]
 	return path
+
 
 func _bfs(node_from, node_to):
 	var node_map = {node_from: null}
