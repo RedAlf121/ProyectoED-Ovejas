07e8f17d423db152de47fc651ad59064877c379e
NeoUroboros <fabioford707@gmail.com>
2023-05-15 20:36:33 -0400

    Ahora si

--- scenes/levels/levelorueba.gd
+++ scenes/levels/levelorueba.gd
@@ -5,7 +5,7 @@ export(String, FILE) var level_selector
 var list = []
 var signal_dictionary = preload("res://singletons/SignalsDictionary.gd")
 onready var item_list = $Canvas/ItemList
-onready var item_list_2 = $Canvas/ItemList2
+onready var item_list_2 = $Canvas/CommandShower
 onready var audio_stream_player = $AudioStreamPlayer
 onready var control_commands = $ControlCommands
 
@@ -15,16 +15,12 @@ func _ready():
 		audio_stream_player.play()
 
 
-func _on_Button_pressed():
-	get_tree().change_scene(level_selector)
+
 
 func _on_ItemList_item_selected(index):
 	_add_to_second_list(index)
 
 
-func _on_ItemList2_item_selected(index):
-	item_list_2.remove_item(index)
-	list.pop_at(index)
 
 func _add_to_second_list(index):
 	var signal_name = signal_dictionary.get_signal(index)
@@ -37,3 +33,19 @@ func _add_to_second_list(index):
 
 func start_commands():
 	control_commands.start(list)
+
+
+func _on_Atras_pressed():
+	get_tree().change_scene(level_selector)
+	pass # Replace with function body.
+
+
+func _on_Reset_pressed():
+	get_tree().change_scene("res://scenes/levels/levelorueba.tscn")
+	pass # Replace with function body.
+
+
+func _on_CommandShower_item_selected(index):
+	item_list_2.remove_item(index)
+	list.pop_at(index)
+
